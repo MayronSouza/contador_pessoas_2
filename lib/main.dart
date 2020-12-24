a971fb2e09f322384e141abb5997b398603c7dc4
MayronSouza <mayron.souza@gmz.solutions>
2020-12-23 22:38:52 -0300

    Criando uma coluna que vai conter os widgets do nosso App

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Contador de Pessoas 2',
-    home: Container(
-      color: Colors.white,
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Pessoas: 0',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Pode Entrar!',
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 32.0,
+          ),
+        ),
+      ],
     ),
     debugShowCheckedModeBanner: false,
   ));
